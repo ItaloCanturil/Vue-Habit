a6a5853f6f562f3780a486900b99afc3f889c43b
Canturilsnaik <italo.cantur@gmail.com>
2021-03-10 10:34:01 -0300

    Add functions

--- src/App.vue
+++ src/App.vue
@@ -16,14 +16,17 @@ export default {
 }
 </script>
 
-<styl
-    HabitTrackere>
+<style>
+body {
+  background: #0D243C;
+  color: #9EA7B1;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
--- src/components/HabitTracker.vue
+++ src/components/HabitTracker.vue
@@ -5,13 +5,19 @@
     </header>
 
     <div class="date">
-      <label for="date__data">{{ date }}</label>
+      <label for="date__data">Date: </label>
       <input
         class="date__data"
         type="date"
         name="date__data"
+        v-model="date"
       >
     </div>
+
+    <section class="modal">
+      <p class="modal__add" @click="searchDate()">Add Goal</p>
+      <p class="modal__chart">See the Chart</p>
+    </section>
   </section>  
 </template>
 
@@ -20,12 +26,29 @@ export default {
   data () {
     return {
       title: 'Habit Tracker',
-      date: 'Date:' 
+      date: '' 
+    }
+  },
+
+  methods: {
+    searchDate () {
+      if(!this.date){
+        alert('Put the date')
+        return
+      }
     }
   }
 }
 </script>
 
 <style>
+.modal {
+  display: flex;
+  justify-content: center;
+}
 
+.modal__add {
+  margin-right: 10px;
+  cursor: pointer;
+}
 </style>
