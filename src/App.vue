e150d5b88e9d08a2d220ffab83fa2bfd47f4588b
Canturilsnaik <italo.cantur@gmail.com>
2021-03-09 23:26:49 -0300

    :tada: Begin the project

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HabitTracker/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HabitTracker from './components/HabitTracker'
 
 export default {
-  name: 'App',
+  name: 'habit-tracker',
+
   components: {
-    HelloWorld
+    HabitTracker
   }
 }
 </script>
 
-<style>
+<styl
+    HabitTrackere>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/HabitTracker.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="habit-tracker">
+    <header>
+      <h1>{{ title }}</h1>
+    </header>
+
+    <div class="date">
+      <label for="date__data">{{ date }}</label>
+      <input
+        class="date__data"
+        type="date"
+        name="date__data"
+      >
+    </div>
+  </section>  
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Habit Tracker',
+      date: 'Date:' 
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
