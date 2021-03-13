48bd9a5ab6249cef75a8d8d105e44ac85d64b1b3
Canturilsnaik <italo.cantur@gmail.com>
2021-03-12 23:15:58 -0300

    Card component init

---
+++ src/components/Card.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="card">
+    <p>{{ title }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
