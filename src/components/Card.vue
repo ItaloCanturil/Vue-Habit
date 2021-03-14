d574db2eeada0cf0f685bd34d508f71d1c334c9b
Canturilsnaik <italo.cantur@gmail.com>
2021-03-13 21:34:39 -0300

    Add img for the cards

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="card">
     <p>{{ title }}</p>
+    <img src="../assets/download_done.png" alt="Confirm the habit">
+    <img src="../assets/edit.png" alt="Edit the habit">
   </div>
 </template>
 
