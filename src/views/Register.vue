5955daa1b4f4472fb1a6191eab7279ae160e46b7
Canturilsnaik <italo.cantur@gmail.com>
2021-03-17 23:57:30 -0300

    Add the register page

---
+++ src/views/Register.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="register">
+    <h1 class="register__title">Sign Up</h1>
+    <fieldset class="register__field">
+      <form class="field__form">
+        <input type="text" class="form__name" placeholder="Name">
+        <input type="email" class="form__email" placeholder="Email">
+        <input type="password" class="form__password" placeholder="Password">
+      </form>
+      <a href="/">Click here for login</a>
+      <button>Register</button>
+    </fieldset>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Login',
+}
+</script>
+
+<style scoped>
+.register {
+  background: #0d243c;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.register__field {
+  background: #f2f2f2;
+  border-radius: 8px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 350px;
+  width: 300px;
+}
+</style>
