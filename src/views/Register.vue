154a6e1a3c53b8fbd7d7fb98a0d1f43b395b0c0b
Canturilsnaik <italo.cantur@gmail.com>
2021-03-20 23:43:17 -0300

    Add axios

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -3,20 +3,58 @@
     <h1 class="register__title">Sign Up</h1>
     <fieldset class="register__field">
       <form class="field__form">
-        <input type="text" class="form__name" placeholder="Name">
-        <input type="email" class="form__email" placeholder="Email">
-        <input type="password" class="form__password" placeholder="Password">
+        <input type="text" v-model="name" class="form__name" placeholder="Name">
+        <input type="email" v-model="email" class="form__email" placeholder="Email">
+        <input type="password" v-model="password" class="form__password" placeholder="Password">
       </form>
       <a href="/">Click here for login</a>
-      <button>Register</button>
+      <button @click="register()">Register</button>
     </fieldset>
+
+    <footer class="register__footer">
+      <figure class="footer__fig">
+        <img class="fig__wave" src="../assets/wave.png" alt="Wave">
+      </figure>
+    </footer>
   </div>
 </template>
 
 <script>
-
+import axiosInstance from '../../services/api'
 export default {
-  name: 'Login',
+  name: 'Register',
+
+  data () {
+    return {
+      name: '',
+      email: '',
+      password: ''
+    }
+  },
+
+  methods: {
+    async register () {
+      try {
+        await axiosInstance.post('/register', {
+          name: this.name,
+          email: this.email,
+          password: this.password
+        })
+          this.$router.push('/')
+      } catch(error) {
+          if (error.response) {
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+          } else if (error.request) {
+            console.log(error.request)
+          } else {
+            console.log('Error', error.message)
+          }
+        console.log(error)
+      }
+    }
+  }
 }
 </script>
 
@@ -43,5 +81,19 @@ export default {
   align-items: center;
   height: 350px;
   width: 300px;
+}
+
+.fig__wave {
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  z-index: -999;
+}
+
+.field__form {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 }
 </style>
