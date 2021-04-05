95391ff27fa4f9f082796616de2a6cece772b59f
Canturilsnaik <italo.cantur@gmail.com>
2021-04-05 11:28:51 -0300

    UI: add the ui for the page home

--- src/views/PageHome.vue
+++ src/views/PageHome.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <header class="home__header">
-      <router-link to="/home">
+      <router-link to="/home" class="header__logo">
         <h1>Habit Tracker</h1>
       </router-link>
       <div class="home__sign">
@@ -17,6 +17,16 @@
         </router-link>
       </div>
     </header>
+
+    <section class="home__apresentation">
+      <p class="apresentation__text">
+        Defina suas metas para atingir seus objetivos
+      </p>
+
+      <figure class="apresentation__map">
+        <img class="map__img" src="../assets/undraw_adventure_map_hnin(1).svg" alt="Adventure Map">
+      </figure>
+    </section>
   </div>
 </template>
 
@@ -31,11 +41,54 @@ export default {
   align-items: center;
   background: #1E6091;
   display: flex;
-  height: 60px;
+  height: 50px;
   justify-content: space-between;
   position: fixed;
   top: 0;
   left: 0;
   width: 100%;
+}
+
+.home__apresentation {
+  display: flex;
+  justify-content: space-evenly;
+  width: 100vw; 
+}
+
+.apresentation__text{
+  color: #f2f2f2;
+  font-size: clamp(2rem, 5.5vw, 4rem);
+  line-height: 90px;
+  max-width: 500px;
+  text-align: center;
+}
+
+.map__img {
+  height: 425px;
+  width: 100%;
+  max-width: 548px;
+}
+
+.home__sign {
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  width: 180px;
+}
+
+.sign__login {
+  text-decoration: none;
+  color: #f1f3f3;
+}
+
+.sign__register {
+  background: #FF8500;
+  border-radius: 10px;
+  padding: 5px 10px;
+}
+
+.header__logo, .sign__register {
+  color: #f2f2f2;
+  text-decoration: none;
 }
 </style>
